9953f81694ae45f21b48e9d9e2452a80b560a1c8
pixelgame239 <pixelgame2004@outlook.com.vn>
2025-02-15 17:42:42 +0700

    add Online Takeaway screen

--- src/pages/OnlineTakeaway.jsx
+++ src/pages/OnlineTakeaway.jsx
@@ -1,8 +1,86 @@
 const  OnlineTakeAway = ()=>{
     return <div>
         <h2>Đồ ăn🍕</h2>
-        <div></div>
+        <div style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+        }}>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                    <p style={{display: "inline-block"}}>Số lượng: <strong>0</strong></p>
+                    <button style={{display: "inline-block", backgroundColor: "lightblue", marginLeft:20}}>➖</button>
+                    <button style={{display: "inline-block", backgroundColor: "lightblue", marginLeft:20}}>➕</button>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+                <div style={{margin:10}}>
+                    <img src="https://via.placeholder.com/300"></img>
+                    <p>Pizza có dứa (không dành cho người Ý)</p>
+                    <p>Giá: 80.000đ</p>
+                </div>
+            </div>
         <h2>Đồ uống 🧋</h2>
+        <div style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+        }}>
+            <div style={{margin:10}}>
+                <img src="https://via.placeholder.com/300"></img>
+                <p>Nước NH3 </p>
+                <p>Giá: 30.000đ</p>            
+            </div>
+            <div style={{margin:10}}>
+                <img src="https://via.placeholder.com/300"></img>
+                <p>Nước NH3 </p>
+                <p>Giá: 30.000đ</p>            
+            </div>
+            <div style={{margin:10}}>
+                <img src="https://via.placeholder.com/300"></img>
+                <p>Nước NH3 </p>
+                <p>Giá: 30.000đ</p>            
+            </div>
+            <div style={{margin:10}}>
+                <img src="https://via.placeholder.com/300"></img>
+                <p>Nước NH3 </p>
+                <p>Giá: 30.000đ</p>            
+            </div>
+            <div style={{margin:10}}>
+                <img src="https://via.placeholder.com/300"></img>
+                <p>Nước NH3 </p>
+                <p>Giá: 30.000đ</p>            
+            </div>
+        </div>
+        <button style={{bottom:20, right: 20, position: "fixed", color: "green", backgroundColor: "white", borderColor: "blue", padding: 20}}>Đặt</button>
     </div>
 }
 export default OnlineTakeAway;
