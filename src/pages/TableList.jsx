ea151947a5be0a7b0bf79af9b47faf60d16194a6
Dang Hoang <hdjhdbsudg@gmail.com>
2025-02-17 16:32:34 +0700

    authenticate login and fetch Account

--- src/pages/TableList.jsx
+++ src/pages/TableList.jsx
@@ -3,22 +3,26 @@ import { Table } from "../backend/tableData";
 import Tables from "../components/Tables";
 import OnlineOrderButton from "../components/OnlineOrderButton";
 
-const TableList = () =>{
-    const [tableData, setTableData] = useState([]);
+const TableList = () => {
+  const [tableData, setTableData] = useState([]);
 
-    useEffect(() => {
-        const table = new Table();
-        table.getTableData().then((data) => {
-            setTableData(data);
-        });
-    }, []);
-    return(
-        <div>
-            <OnlineOrderButton></OnlineOrderButton>
-            {tableData.map((table, index) => (
-                <Tables key={index} tableID={table.tableID} isOccupied={table.isOccupied} />
-            ))}
-        </div>
-    )
-}
+  useEffect(() => {
+    const table = new Table();
+    table.getTableData().then((data) => {
+      setTableData(data);
+    });
+  }, []);
+  return (
+    <div style={{ textAlign: "center" }}>
+      <OnlineOrderButton></OnlineOrderButton>
+      {tableData.map((table, index) => (
+        <Tables
+          key={index}
+          tableID={table.tableID}
+          isOccupied={table.isOccupied}
+        />
+      ))}
+    </div>
+  );
+};
 export default TableList;
