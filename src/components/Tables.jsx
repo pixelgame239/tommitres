c6076010cecdea5d16ceec1f769451c0fe6ca972
pixelgame239 <pixelgame2004@outlook.com.vn>
2025-03-11 21:22:16 +0700

    11/3 table Realtime

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -4,30 +4,41 @@ import table_full from "../assets/table_full.png"
 import { Table } from "../backend/tableData";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { collection, onSnapshot, deleteDoc, doc, getDocs, orderBy, query, where } from "firebase/firestore";
+import { db } from "../backend/firebase";
 
 function Tables(tabledata) {
     const navigate = useNavigate();
-    const [status, setStatus]= useState(tabledata.isOccupied);
+    const [status, setStatus]= useState();
+    const [tableID, setTableID] = useState();
     const handleStatus = async () => {
         try {
             const currentTable = new Table();
-            await currentTable.changeTableData(tabledata.tableID, tabledata.isOccupied);
+            await currentTable.changeTableData(tableID, status);
             setStatus((prevStatus) => !prevStatus);
         } catch (error) {
             console.error("Error updating table status:", error);
         }
     };
     const handleOrder = async() =>{
-        if(!tabledata.isOccupied){
+        if(!status){
             await handleStatus();
         }
         navigate(`/tommitres/Order/Table${tabledata.tableID}`);
     }
     useEffect(() => {
-        setStatus(tabledata.isOccupied); 
-    }, [tabledata.isOccupied]);
+        const unsubscribe = onSnapshot(query(collection(db, "Table"), where("tableID","==", tabledata.tableID)),
+    (snapshot)=>{
+        const thisTable = snapshot.docs[0];
+        const thistableID = thisTable.data().tableID;
+        const thisStatus = thisTable.data().isOccupied;
+        setStatus(thisStatus);
+        setTableID(thistableID);
+        return()=>unsubscribe();
+    });
+    }, [status]);
     return <div className="table">
-        <h2 className="table-number">{tabledata.tableID}</h2>
+        <h2 className="table-number">{tableID}</h2>
         <img className = "table-display" src= {status ? table_full : table_empty}></img>
         <p>Trạng thái:</p>
         <p className={status ?"table-status-full":"table-status"}>{status ? "Đang sử dụng" : "Trống"}</p>
