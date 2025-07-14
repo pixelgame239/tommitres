a8954beec66d0a88ae39348bcf37e85553e1d9b5
pixelgame239 <117742483+pixelgame239@users.noreply.github.com>
2025-07-14 10:12:01 +0700

    Update firebase.js

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
+  apiKey: "AIzaSyAuqKhjCSWU_E_ubt4jPpTVX4iv4H75WeY",
   authDomain: "tommitrestaurant.firebaseapp.com",
   projectId: "tommitrestaurant",
   storageBucket: "tommitrestaurant.firebasestorage.app",
